4eda17b6dcafde4dd49dd49a3659f25b7895a272
ArmineHam <arminehambardzumyan@yahoo.com>
2022-01-11 15:22:26 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '2fd69fe2-9813-4dd7-8716-d13aa91d6216';
+export const MODAL_CLOSE_SEC = 2.5;
